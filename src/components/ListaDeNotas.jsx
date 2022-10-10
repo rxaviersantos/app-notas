d24e72cba11664eb76bd010e397937344fc40db9
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-10 16:33:42 -0300

    create pasta components

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export default class ListaDenotas extends Component {
+
+    render(){
+        return(
+            <ul> 
+            <li>
+              <section>
+                <header>
+                  <h3>Título</h3>
+                </header>
+                <p>Escreva sua nota</p>
+              </section>
+            </li>
+            <li>
+              <section>
+                <header>
+                  <h3>Título</h3>
+                </header>
+                <p>Escreva sua nota</p>
+              </section>
+            </li>
+            <li>
+              <section>
+                <header>
+                  <h3>Título</h3>
+                </header>
+                <p>Escreva sua nota</p>
+              </section>
+            </li>
+          </ul>
+        );
+    }
+
+}
