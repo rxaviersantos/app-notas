40d8fbe7273cb1aa559999f137593ea53b59aa00
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2022-10-10 23:11:35 -0300

    integrando css dentro da aplicação

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import FormularioCadastro  from "./components/FormularioCadastro";
-import ListaDenotas from "./components/ListaDeNotas";
-
+import ListaDenotas from "./components/ListaDeNotas/ListaDeNotas";
+import "./App.css"
 class App extends Component{
   render(){
     return (
---
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import CardNota from "../CardNota/CardNota";
+import "./estilo.css"
+
+class ListaDeNotas extends Component {
+
+    render(){
+        return(
+            <ul className="lista-notas"> 
+              {Array.of("Trabalho", "Trabalho", "Estudos").map((index) => {
+                return (
+            <li className="lista-notas_item" key={index}>
+
+              <CardNota/>
+            </li>
+                )
+              })}
+          </ul>
+        );
+    }
+
+}
+
+export default ListaDeNotas;
